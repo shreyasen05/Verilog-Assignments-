218455417bc38fdd44bfac81cff631e3589f5943
shreyasen05 <shreyasen793@gmail.com>
2026-09-07 00:18:23 +0530

    Document shift_rows module for S-AES
    Added module documentation for shift_rows.

--- Simplified Advanced Encryption Standard (S-AES)/shift_rows.v
+++ Simplified Advanced Encryption Standard (S-AES)/shift_rows.v
@@ -1,6 +1,21 @@
-module shift_rows (in, out);
-    input [15:0]  in;
-    output [15:0]  out;
+//////////////////////////////////////////////////////////////////////////////////
+// Module Name : shift_rows
+// Project Name : Simplified Advanced Encryption Standard (S-AES)
+//
+// Description :
+//   This module is used to rearranges the four nibbles of the 
+//   16-bit state according to the S-AES ShiftRows operation.
+//   Initial Position : N00 N01 N10 N11 
+//   Final Position : N00 N01 N11 N10
+//
+// Student Name : Shreya Sen
+// S-ID : 231001002086
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module shift_rows (
+    input [15:0]  in,
+    output [15:0]  out
+);
     assign out = {in[15:12], in[11:8], in[3:0], in[7:4]};
 endmodule
